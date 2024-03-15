d30029268379abb417ee62bb1e466ced1ce5b723
Anne-Flore BERNARD <anne-flore.bernard@FRARTL2TDQJ9D16-M.local>
2024-03-15 11:34:56 +0100

    logic+style : filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+
 
   return (
     <>
-      <div></div>
-      <h1>Vite + React</h1>
+      <div>
+  
+      </div>
+     
+      
 
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+  
     </>
   );
 }
--- src/design-system/HomePage/Filter/Filter.tsx
+++ src/design-system/HomePage/Filter/Filter.tsx
@@ -1,12 +1,65 @@
 import { FC } from "react";
+import { useState } from "react";
 
 type FilterProps = {
-  label: string;
-  ternaryFilter: boolean;
-  types: Array<{ id: number; name: string; url: string }>;
+  // date: string;
+  // ternaryFilter: boolean;
+  // types: Array<{ id: number; name: string; url: string }>;
 };
 
 const Filter: FC<FilterProps> = ({}) => {
-  return <div></div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const handleIsOpen = () => setIsOpen(!isOpen);
+  return (
+    <div onClick={handleIsOpen} className="relative inline-block text-left">
+      <div>
+        <button
+          type="button"
+          className="inline-flex w-full justify-center gap-x-1.5 rounded-lg bg-blue-900 px-3 py-2 text-sm font-semibold text-blue-200 shadow-sm ring-1 ring-inset ring-blue-300 hover:bg-blue-200 hover:text-blue-900"
+          id="menu-button"
+          aria-expanded={isOpen}
+        >
+          2015
+          <svg
+            className="-mr-1 h-5 w-5 text-gray-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <>
+        {isOpen ? (
+          <ul className={` border absolute top-20 z-10`}>
+            <li
+              className={`relative w-[122px] h-[80px] mx-4 pl-4 grid grid-cols-2 font-light place-content-center  box-border`}
+            >
+              yo
+            </li>
+            <li
+              className={`relative w-[122px] h-[80px] mx-4 pl-4 grid grid-cols-2 font-light place-content-center  box-border`}
+            >
+              yaya
+            </li>
+            <li
+              className={`relative w-[122px] h-[80px] mx-4 pl-4 grid grid-cols-2 font-light place-content-center  box-border`}
+            >
+              hfvizhv
+            </li>
+          </ul>
+        ) : (
+          ""
+        )}
+      </>
+    </div>
+  );
 };
+
 export default Filter;
