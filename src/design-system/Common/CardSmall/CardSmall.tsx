c21f7eb16d1a728279f9bee085c9b1619be27dd7
Anne-Flore Bernard <anne-flore.bernard@artefact.com>
2024-03-21 09:54:11 +0100

    fix: api et fetch => cardSmall

--- src/design-system/Common/CardSmall/CardSmall.tsx
+++ src/design-system/Common/CardSmall/CardSmall.tsx
@@ -1,7 +1,15 @@
-import { FC } from "react";
+import {
+  FC,
+  JSXElementConstructor,
+  Key,
+  ReactElement,
+  ReactNode,
+  ReactPortal,
+} from "react";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { log } from "echarts/types/src/util/log.js";
+// import { log } from "echarts/types/src/util/log.js";
+import Movie from "../../../pages/Movie";
 
 type Movie = {
   id: number;
@@ -20,7 +28,8 @@ const CardSmall: FC<CardSmallProps> = () => {
       method: "GET",
       headers: {
         accept: "application/json",
-        Authorization: "Bearer 2af23d639764adc4f8c8743b94600647",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZGJlMWI0YTlhZjMwNDI4MGE1MDUwYWZmY2NiZmZiOSIsInN1YiI6IjY1ZmJlZWYyNjA2MjBhMDE3YzI2ZTNiYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KPAXlN8V9CgYtRWiFz-jSGPAOwHo_JkUauM5q2rl9Lk",
       },
     };
 
@@ -30,14 +39,12 @@ const CardSmall: FC<CardSmallProps> = () => {
     )
       .then((response) => response.json())
       .then((response) => {
-        const loadedMovies: Movie[] = response.results.map((movie: any) => [
-          {
-            id: movie.id,
-            title: movie.title,
-            release_date: movie.release_date,
-            poster_path: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-          },
-        ]);
+        const loadedMovies: Movie[] = response.results.map((movie: any) => ({
+          id: movie.id,
+          title: movie.title,
+          release_date: movie.release_date,
+          poster_path: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+        }));
         setMovies(loadedMovies);
         console.log(movies);
       })
@@ -46,41 +53,63 @@ const CardSmall: FC<CardSmallProps> = () => {
 
   return (
     <div>
-      {movies.map((movie) => (
-        <div
-          key={movie.id}
-          className=" max-w-sm rounded-lg overflow-hidden shadow-lg w-[260px] min-w-[284px] h-[380px] min-h-[380px] bg-blue-100 mb-4"
-        >
-          <div>
-            <img
-              className=" h-[240px] min-w-[240px] w-full  object-cover"
-              src={movie.poster_path}
-              alt={movie.title}
-            ></img>
-          </div>
-          <div className="flex items-center justify-between">
-            <div className="px-6 py-4 flex flex-col items-start">
-              <div id="title" className="font-bold text-xl mb-2">
-                {movie.title}
-              </div>
-              <p id="year" className="text-gray-700 text-base">
-                {movie.release_date}
-              </p>
-            </div>
-            <div className="px-6  pb-4 flex flex-row-reverse ">
-              <span className=" bg-white rounded-full py-2 px-1 text-sm font-semibold text-red-800 mr-2 mb-2">
-                +120 KS
-              </span>
-            </div>
-          </div>
-          <Link
-            to="/Movie"
-            className="text-blue-900 bg-blue-400 ring-1 ring-inset ring-blue-300 rounded-full px-4 py-0 lg:py-1 border border-blue-300 hover:text-blue-200 hover:bg-blue-900 flex justify-center mx-20 font-medium text-base"
+      {movies.map(
+        (movie: {
+          id: Key | null | undefined;
+          poster_path: string | undefined;
+          title:
+            | string
+            | number
+            | boolean
+            | ReactElement<any, string | JSXElementConstructor<any>>
+            | Iterable<ReactNode>
+            | null
+            | undefined;
+          release_date:
+            | string
+            | number
+            | boolean
+            | ReactElement<any, string | JSXElementConstructor<any>>
+            | Iterable<ReactNode>
+            | ReactPortal
+            | null
+            | undefined;
+        }) => (
+          <div
+            key={movie.id}
+            className=" max-w-sm rounded-lg overflow-hidden shadow-lg w-[260px] min-w-[284px] h-[380px] min-h-[380px] bg-blue-100 mb-4"
           >
-            Détails
-          </Link>
-        </div>
-      ))}
+            <div>
+              <img
+                className=" h-[240px] min-w-[240px] w-full  object-cover"
+                src={movie.poster_path}
+                // alt={movie.title}
+              ></img>
+            </div>
+            <div className="flex items-center justify-between">
+              <div className="px-6 py-4 flex flex-col items-start">
+                <div id="title" className="font-bold text-xl mb-2">
+                  {movie.title}
+                </div>
+                <p id="year" className="text-gray-700 text-base">
+                  {movie.release_date}
+                </p>
+              </div>
+              <div className="px-6  pb-4 flex flex-row-reverse ">
+                <span className=" bg-white rounded-full py-2 px-1 text-sm font-semibold text-red-800 mr-2 mb-2">
+                  +120 KS
+                </span>
+              </div>
+            </div>
+            <Link
+              to="/Movie"
+              className="text-blue-900 bg-blue-400 ring-1 ring-inset ring-blue-300 rounded-full px-4 py-0 lg:py-1 border border-blue-300 hover:text-blue-200 hover:bg-blue-900 flex justify-center mx-20 font-medium text-base"
+            >
+              Détails
+            </Link>
+          </div>
+        )
+      )}
     </div>
   );
 };
