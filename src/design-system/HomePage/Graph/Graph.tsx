8f32e56f394bb78cc6bdd9829ad27fdd6aa6424e
Anne-Flore Bernard <anne-flore.bernard@artefact.com>
2024-03-29 15:32:06 +0100

    graph: améliorations

--- src/design-system/HomePage/Graph/Graph.tsx
+++ src/design-system/HomePage/Graph/Graph.tsx
@@ -7,6 +7,10 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  BarChart,
+  Bar,
+  ResponsiveContainer,
+  ComposedChart,
 } from "recharts";
 
 type GraphProps = {
@@ -17,95 +21,69 @@ type GraphProps = {
 
 const data = [
   {
-    name: "Kung Fu Panda",
-    uv: 4000,
-    pv: 2400,
+    name: "2020",
+    "nombre de film": 4000,
+    "note moyenne": 2400,
     amt: 2400,
   },
   {
-    name: "Godzilla",
-    uv: 3000,
-    pv: 1398,
+    name: "2021",
+    "nombre de film": 3000,
+    "note moyenne": 1398,
     amt: 2210,
   },
   {
-    name: "Madame Web",
-    uv: 2000,
-    pv: 9800,
+    name: "2022",
+    "nombre de film": 2000,
+    "note moyenne": 9800,
     amt: 2290,
   },
   {
-    name: "Road House",
-    uv: 2780,
-    pv: 3908,
+    name: "2023",
+    "nombre de film": 2780,
+    "note moyenne": 3908,
     amt: 2000,
   },
   {
-    name: "DamSel",
-    uv: 1890,
-    pv: 4800,
+    name: "2024",
+    "nombre de film": 1890,
+    "note moyenne": 4800,
     amt: 2181,
-  },
-  {
-    name: "Sri Asih",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Alienoid",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-  {
-    name: "Creation of God",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-  {
-    name: "No way up",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-  {
-    name: "Migration",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
   },
 ];
 
 const Graph: FC<GraphProps> = ({}) => {
   return (
-    <div className="w-fit">
-      <LineChart
-        width={350}
-        height={300}
-        data={data}
-        margin={{
-          top: 5,
-          right: 10,
-          left: 10,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line
-          type="monotone"
-          dataKey="pv"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-      </LineChart>
-    </div>
+    <ResponsiveContainer width="100%" height="100%">
+      <div className=" flex justify-center w-screen">
+        <ComposedChart
+          width={350}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 10,
+            left: 10,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="#f5f5f5" />
+          <XAxis dataKey="name" scale="band" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="nombre de film"
+            stroke="#60aedb"
+            activeDot={{ r: 8 }}
+          />
+          {/* <Line type="monotone" dataKey="note moyenne" stroke="#82ca9d" /> */}
+
+          <Bar dataKey="note moyenne" barSize={20} fill="#b35a0c" />
+        </ComposedChart>
+      </div>
+    </ResponsiveContainer>
   );
 };
 export default Graph;
