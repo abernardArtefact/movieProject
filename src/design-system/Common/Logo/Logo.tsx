15eeda7eb4d80882a716ae0920bed9bb4e1c1697
Anne-Flore Bernard <anne-flore.bernard@artefact.com>
2024-04-17 13:32:27 +0200

    moving assets

--- src/design-system/Common/Logo/Logo.tsx
+++ src/design-system/Common/Logo/Logo.tsx
@@ -5,7 +5,7 @@ type LogoProps = {};
 const Logo: FC<LogoProps> = () => {
   return (
     <div className="h-12 min-w-20 w-24 grid content-center lg:h-24 lg:w-32">
-      <img src="src/assets/logo.svg" alt=" Logo" />
+      <img src="/assets/logo.svg" alt=" Logo" />
     </div>
   );
 };
