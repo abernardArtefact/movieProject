359342cfded210310c928b37cabb83a50099728d
Anne-Flore Bernard <anne-flore.bernard@artefact.com>
2024-04-18 15:24:09 +0200

    add: favoriteMovie.model.js

---
+++ server/models/favoriteMovie.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const favoriteMovieSchema = new mongoose.Schema({
+    title: String,
+    director: String,
+    releaseYear: Number,
+    userId: String // ou mongoose.Schema.Types.ObjectId quand je vais gèrer les utilisateurs
+});
+const FavoriteMovie = mongoose.model('FavoriteMovie', favoriteMovieSchema);
+module.exports = FavoriteMovie; 
--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@
 // const yoyoyo = 1;
 // console.log(yoyoyo);
 
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
 
+
+
 mongoose.connect('mongodb+srv://anneflower:hlBOy1tH7iDW498N@backenddb.vo6dccc.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB')
-.then(() => console.log('Connected!'));
+.then(() => console.log('Connected to database!'));
 const app = express()
+
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("ca marche sur 3000");
 })
