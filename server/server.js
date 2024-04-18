56e1c2590be585d4802e8f9438bac5e37aec55dc
Anne-Flore Bernard <anne-flore.bernard@artefact.com>
2024-04-18 14:41:50 +0200

    install: mongoDB, mongoose => test ok, we are connected

--- server/server.js
+++ server/server.js
@@ -1,34 +1,12 @@
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://banneflore:<Pilipili17%>@anne.fbxhmhb.mongodb.net/?retryWrites=true&w=majority&appName=Anne";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
 // const yoyoyo = 1;
 // console.log(yoyoyo);
 
 const express = require('express')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://anneflower:hlBOy1tH7iDW498N@backenddb.vo6dccc.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB')
+.then(() => console.log('Connected!'));
 const app = express()
 app.listen(3000, () => {
     console.log("ca marche sur 3000");
