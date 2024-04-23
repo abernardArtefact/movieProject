de9f2752d0adbd3d6f7d2e41d9280523be166953
Anne-Flore Bernard <anne-flore.bernard@artefact.com>
2024-04-23 18:04:54 +0200

    style responsive register

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -6,25 +6,25 @@ const Register = () => {
   const [password, setPassword] = useState("");
 
   return (
-    <div className="main-container w-full h-full bg-blue-900">
-      <h1 className="text-2xl text-white text-center pt-16">Enregistrement</h1>
-      <form className="py-8 flex flex-col pt-14 lg:pt-40 ">
+    <div className="main-container w-full h-full bg-blue-900 flex justify-center flex-col items-center">
+      <h1 className="text-2xl text-white text-center pt-16 ">Enregistrement</h1>
+      <form className="py-8 flex flex-col pt-14 lg:pt-40 lg:w-[500px] ">
         <input
-          className="bg-blue-700 text-center py-8 mb-4"
+          className="bg-blue-700 text-center py-8 px-12 mb-4 lg:w-full"
           value={name}
           onChange={(event) => setName(event.target.value)}
           type="text"
           placeholder="Your name"
         ></input>
         <input
-          className="bg-blue-700 text-center py-8 mb-4"
+          className="bg-blue-700 text-center py-8 px-12 mb-4 lg:w-full"
           value={email}
           onChange={(event) => setEmail(event.target.value)}
           type="text"
           placeholder="Your email"
         ></input>
         <input
-          className="bg-blue-700 text-center py-8 mb-4"
+          className="bg-blue-700 text-center py-8 mb-4 px-12 lg:w-full"
           value={password}
           onChange={(event) => setPassword(event.target.value)}
           type="text"
